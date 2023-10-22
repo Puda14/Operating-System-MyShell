d9af9014fa7abb48b5e64ca39385c8bad46e9b88
Phùng Tiến Đạt <96523185+Puda14@users.noreply.github.com>
2023-10-22 18:52:57 +0700

    first commit

---
+++ MyShell.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+
+void time_process(){
+	STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    CreateProcess("Timer.exe", NULL, NULL, NULL, FALSE, 
+				CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
+	WaitForSingleObject(pi.hProcess, 10000);
+	TerminateProcess(pi.hProcess, 0);
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+}
+
+void aProcess(){
+	STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    CreateProcess("aProcess.exe", NULL, NULL, NULL, FALSE, 
+				0, NULL, NULL, &si, &pi);
+	WaitForSingleObject(pi.hProcess, 10000);
+	TerminateProcess(pi.hProcess, 0);
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+}
+
+void createDir(){
+	STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    CreateProcess("createDir.exe", NULL, NULL, NULL, FALSE, 
+				0, NULL, NULL, &si, &pi);																																																																																																																																																																																											
+	WaitForSingleObject(pi.hProcess, 10000);
+	TerminateProcess(pi.hProcess, 0);
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+}
+
+void list(){
+	STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    CreateProcess("list.exe", NULL, NULL, NULL, FALSE, 
+				0, NULL, NULL, &si, &pi);																																																																																																																																																																																											
+	WaitForSingleObject(pi.hProcess, 10000);
+	TerminateProcess(pi.hProcess, 0);
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+}
+
+
+
+int main() {
+    char cmd[100];
+
+    while (1) {
+
+        printf("Enter a command: ");
+        scanf("%[^\n]%*c", cmd);
+
+        if(strcmp(cmd, "time") == 0) time_process();
+        else if(strcmp(cmd, "test") == 0) aProcess();
+        else if(strcmp(cmd, "mkdir") == 0) createDir();
+        else if(strcmp(cmd, "ls") == 0) list();
+        else system(cmd);
+    }
+    return 0;
+}
+
---
+++ aProcess.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(){
+	printf("This is a process !\n");
+	return 0;
+}
