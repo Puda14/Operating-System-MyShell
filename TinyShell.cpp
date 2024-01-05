08eb95289d34c24a8fd620360d7f7bc0f79a9775
Phùng Tiến Đạt <96523185+Puda14@users.noreply.github.com>
2024-01-05 22:06:37 +0700

    [fix] get file

--- TinyShell.cpp
+++ TinyShell.cpp
@@ -110,66 +110,85 @@ int main()
 		{
 			listFilesAndDirectories();
 		}
-		else if(strcmp(key, "cd") == 0){
+		else if (strcmp(key, "cd") == 0)
+		{
 			cd(option, buffer);
 		}
-		else if(strcmp(key, "clear") == 0){
+		else if (strcmp(key, "clear") == 0)
+		{
 			system("cls");
 		}
-		else if('A'<=key[0]&&key[0]<='Z'&&key[1] == ':'){
+		else if ('A' <= key[0] && key[0] <= 'Z' && key[1] == ':')
+		{
 			changeDrive(key[0], buffer);
 		}
-		else if(strcmp(key, "mdir") == 0){
+		else if (strcmp(key, "mdir") == 0)
+		{
 			makeDir(option);
 		}
-		else if(strcmp(key, "rmdir") == 0){
+		else if (strcmp(key, "rmdir") == 0)
+		{
 			removeDir(option);
 		}
-		else if(key[0] == '.' && key[1] == '\\'){
+		else if (key[0] == '.' && key[1] == '\\')
+		{
 
 			char *file;
-			strcpy(file, key+2);
+			file = key + 2;
 
-			char* lastDot = strrchr(key, '.');
-			char type[10];// Type of file
-			
-			if (lastDot != nullptr) {
-        		sscanf(lastDot + 1, "%99s", type);  
-   			} else {
-        		printf("Unable to read file type");
-    		}
+			char *lastDot = strrchr(key, '.');
+			char type[10]; // Type of file
 
-			if(strcmp(type, "exe") == 0) runExe(file, option);
-			else if(strcmp(type, "bat") == 0){
+			if (lastDot != nullptr)
+			{
+				sscanf(lastDot + 1, "%99s", type);
+			}
+			else
+			{
+				printf("Unable to read file type");
+			}
+
+			if (strcmp(type, "exe") == 0)
+				runExe(file, option);
+			else if (strcmp(type, "bat") == 0)
+			{
 				runBat(file);
 			}
 		}
-		else if(strcmp(key, "echo") == 0){
-			
-			if(option[0] == '%' && option[strlen(option)-1] == '%'){
+		else if (strcmp(key, "echo") == 0)
+		{
+
+			if (option[0] == '%' && option[strlen(option) - 1] == '%')
+			{
 				while (option[0] == '%')
 					strcpy(&option[0], &option[1]);
 				while (option[strlen(option) - 1] == '%')
 					strcpy(&option[strlen(option) - 1], &option[strlen(option)]);
 				readEnvironmentVariable(option);
 			}
-			else{
+			else
+			{
 				printf("%s\n", option);
 			}
 		}
-		else if(strcmp(key, "crf") == 0){
+		else if (strcmp(key, "crf") == 0)
+		{
 			createFile(option);
 		}
-		else if(strcmp(key, "delf") == 0){
+		else if (strcmp(key, "delf") == 0)
+		{
 			deleteFile(option);
 		}
-		else if(strcmp(key, "help") == 0){
+		else if (strcmp(key, "help") == 0)
+		{
 			help();
 		}
-		else if(strcmp(key, "exit") == 0){
+		else if (strcmp(key, "exit") == 0)
+		{
 			return 0;
 		}
-		else{
+		else
+		{
 			printf("\x1b[31m");
 			printf("The term %s is not recognized as the name of a cmdlet\n", key);
 			printf("You should use \"help\"\n");
@@ -178,26 +197,27 @@ int main()
 	}
 }
 
-void help() {
+void help()
+{
 	printf("\x1b[34m");
-    printf("Tiny Shell Usage:\n");
-    printf("  list                : List all processes\n");
-    printf("  b                   : Create process in background mode\n");
-    printf("  f                   : Create process in foreground mode\n");
-    printf("  killA [-d]          : Kill all processes (optionally display details with -d)\n");
-    printf("  kill [process name] : Kill a specific process\n");
-    printf("  ls                  : List files and directories in the current directory\n");
-    printf("  cd [directory]      : Change current directory to [directory]\n");
-    printf("  clear               : Clear the console screen\n");
-    printf("  [Drive]:            : Change current drive to [Drive]\n");
-    printf("  mdir [directory]    : Create a new directory named [directory]\n");
-    printf("  rmdir [directory]   : Remove the directory named [directory]\n");
-    printf("  .\\file [-f|-b]      : Execute a file specified by .\\file with optional -f (foreground) or -b (background) option\n");
-    printf("  echo [option]       : Display [option] or read an environment variable\n");
-    printf("  crf [filename]      : Create a new file named [filename]\n");
-    printf("  delf [filename]     : Delete the file named [filename]\n");
+	printf("Tiny Shell Usage:\n");
+	printf("  list                : List all processes\n");
+	printf("  b                   : Create process in background mode\n");
+	printf("  f                   : Create process in foreground mode\n");
+	printf("  killA [-d]          : Kill all processes (optionally display details with -d)\n");
+	printf("  kill [process name] : Kill a specific process\n");
+	printf("  ls                  : List files and directories in the current directory\n");
+	printf("  cd [directory]      : Change current directory to [directory]\n");
+	printf("  clear               : Clear the console screen\n");
+	printf("  [Drive]:            : Change current drive to [Drive]\n");
+	printf("  mdir [directory]    : Create a new directory named [directory]\n");
+	printf("  rmdir [directory]   : Remove the directory named [directory]\n");
+	printf("  .\\file [-f|-b]      : Execute a file specified by .\\file with optional -f (foreground) or -b (background) option\n");
+	printf("  echo [option]       : Display [option] or read an environment variable\n");
+	printf("  crf [filename]      : Create a new file named [filename]\n");
+	printf("  delf [filename]     : Delete the file named [filename]\n");
 	printf("  stop [processname]  : Stop the process named [processname]\n");
-    printf("  resume [processname]: Resume the process named [processname]\n");
-    printf("  exit                : Exit the shell\n");
+	printf("  resume [processname]: Resume the process named [processname]\n");
+	printf("  exit                : Exit the shell\n");
 	printf("\x1b[0m");
 }
